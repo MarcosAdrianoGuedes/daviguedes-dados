1e58a6064e6115dbfb83e11a08c43db6f782102c
MarcosAdrianoGuedes <marcosadri551@gmail.com>
2024-10-27 19:06:36 -0300

    testando

---
+++ src/App.vue
@@ -0,0 +1,156 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+// Variáveis reativas para email e senha
+const email = ref('')
+const password = ref('')
+
+// Funções para manipular o envio do email e senha
+const submitEmail = () => {
+  console.log("Email enviado:", email.value)
+}
+
+const submitLogin = () => {
+  console.log("Email Enviado:", email.value)
+}
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="DAVI GUEDES - ANALISE DE DADOS" />
+
+      <nav>
+        <!-- Formulário de login abaixo dos links -->
+        <div class="login-form">
+          <h2>Faça seu login</h2>
+          <label for="email" required>Email:</label>
+          <input type="email" v-model="email" id="email" placeholder="Digite seu email..." class="email" />
+
+          <label for="password">Senha:</label>
+          <input type="password" v-model="password" id="password" placeholder="Digite sua senha..." class="password" />
+
+        </div>
+        <div class="container">
+          <button type="submit" form="login-form" onclick="login-form" class="login-button">ENTRAR</button>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-button {
+  border-radius: 12px;
+  border: none;
+  color: white;
+  background-color: #00BD7E;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.email,
+.password {
+  border-radius: 12px;
+  border: none;
+  padding: 16px;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+.login-form {
+  margin-top: 20px;
+  text-align: center;
+  border-radius: 12px;
+}
+
+label {
+  display: block;
+  margin: 10px 0 5px;
+}
+
+input {
+  margin-bottom: 10px;
+  padding: 8px;
+  width: 100%;
+  max-width: 300px;
+}
+
+button {
+  margin-bottom: 15px;
+  padding: 8px 16px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-end;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+
+
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,70 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Analise de dados</template>
+
+    <a href="">Ferramenta poderosa</a> que permite transformar grandes volumes de dados brutos em informações
+    estratégicas e
+    <a href="">insights acionáveis</a> para embasar decisões empresariais.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Calculadora para caixas</template>
+
+    <a href="">Otimizar o armazenamento e transporte</a>, calculando automaticamente o volume de produtos para garantir
+    <a href="">o
+      melhor
+      aproveitamento de espaço nas caixas</a>. Este sistema utiliza as dimensões dos produtos (comprimento, largura e
+    altura).
+
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Integração com API para requerimentos</template>
+
+    <a href="">Permite que sistemas interajam com um serviço externo</a> para gerenciar e manipular informações
+    relacionadas a
+    requerimentos. Este tipo de integração é crucial para automatizar processos.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Chamada via e-mail</template>
+
+    <a href="">Funcionalidade que permite a comunicação automatizada com usuários ou clientes</a> através do envio de
+    mensagens eletrônicas. Esse recurso é frequentemente utilizado em sistemas de gerenciamento de requerimentos,
+    notificações, marketing e suporte ao cliente.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Suporte com banco de dados</template>
+    <a href="">Funcionalidade essencial em sistemas de informação que permite o armazenamento, gerenciamento e
+      recuperação
+      de dados de forma eficiente e segura.</a> Essa infraestrutura é fundamental para garantir a integridade,
+    disponibilidade e acessibilidade das informações utilizadas em aplicações empresariais.
+  </WelcomeItem>
+</template>
